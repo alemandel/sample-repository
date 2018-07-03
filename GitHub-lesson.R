ffb9cfe5aad4606703d5367b307629d059963482
Ale Mandel <mandel1@email.arizona.edu>
2018-07-03 10:01:48 -0700

    Add plot script

---
+++ GitHub-lesson.R
@@ -0,0 +1,10 @@
+#' GitHub lesson
+#' create script in R to store in GitHub
+#' Ale Mandel
+#' mandel1@email.arizona.edu
+#'2018-07-03
+#'---------------------------------------------------------------------
+
+x = rnorm(n = 100)
+y = rnorm(n = 100, mean = 2, sd = 4)
+plot(x, y)
